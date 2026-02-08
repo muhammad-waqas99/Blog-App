71b2a50b3905d75c68790280b5eb6d51b147d274
Muhammad Waqas <juniorscript.99@gmail.com>
2026-02-08 23:59:36 +0500

    setup user model and ejs

---
+++ models/User.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const {createHmac,randomBytes} =require('crypto')
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required:true
+
+},
+
+  email:{
+    type:String,
+    required:true
+  },
+
+  salt:{
+    type:String,
+    required:true
+  },
+  password:{
+    type:String,
+    required:true
+  },
+
+  userProfileImage:{
+    type:String,
+    default:"../public/images/Use"
+  }
+
+},{timestamps:true})
+
+const User =mongoose.model("user", userSchema)
+
+module.exports =User
+
+mongoose.pre('save',function (next){
+    const user =this;
+    if(!user.isModified("password")) return;
+
+    const salt =randomBytes(16).toString()
+    const hashPassword =createHmac("sha256",salt)
+    .update(user.password)
+    .digest("hex")
+
+    this.salt =salt
+    this.password=hashPassword
+
+    next()
+
+})
